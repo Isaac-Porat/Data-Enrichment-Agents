afffebe465c20228cd97107429ba523545cd5ee4
ZakPorat <zakporat2023@gmail.com>
2024-10-23 19:21:29 -0400

    Added better checking functionality

--- app/helper_functions/url_checker.py
+++ app/helper_functions/url_checker.py
@@ -1,19 +1,44 @@
 import os
 import pandas as pd
 from dotenv import load_dotenv
+import validators
+from urllib.parse import urlparse
 
 load_dotenv()
 
 SPREADSHEET_PATH = os.getenv("CSV_PATH")
 
-def url_checker():
+def validate_url(url: str) -> bool:
+    try:
+        result = urlparse(url)
+
+        # Check to see if valid URL with scheme and netloc (domain)
+        if result.scheme and result.netloc:
+            validation = validators.url(url) # validators package for stricter validation
+            if validation:
+                return True
+
+        # If not valid URl, check if it's a valid path
+        elif result.path:
+            if result.path.startswith("/") and " " not in result.path:
+                return True
+
+        return False
+
+    except Exception:
+        return False
+
+def loop_through_csv():
     try:
         df = pd.read_csv(SPREADSHEET_PATH)
 
         for index, row in df.iterrows():
             value = row["Company URL"]
 
-            df.at[index, "Company URL"] = f"https://{value}"
+            if validate_url(value):
+                print(value)
+            else:
+                df.at[index, "Company URL"] = f"https://{value}"
 
         df.to_csv(SPREADSHEET_PATH, index=False)
 
@@ -21,4 +46,4 @@ def url_checker():
         print(str(e))
 
 if __name__ == "__main__":
-    url_checker()
+    loop_through_csv()
